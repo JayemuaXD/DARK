4c73acbc7b6fe239cdb791ead3753a7612116967
JayemuaXD <113012220+JayemuaXD@users.noreply.github.com>
2026-08-19 21:04:28 -0500

    Try using fully-qualified Material.STONE in Properties.of to construct Block properties

--- DARK/src/main/java/com/jayemuaxd/dark/core/registration/ModBlocks.java
+++ DARK/src/main/java/com/jayemuaxd/dark/core/registration/ModBlocks.java
@@ -3,8 +3,8 @@ package com.jayemuaxd.dark.core.registration;
 import com.jayemuaxd.dark.core.DarkCoreMod;
 import com.jayemuaxd.dark.core.block.DarkCalderoBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,7 +14,7 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, DarkCoreMod.MODID);
 
     public static final RegistryObject<Block> CALDERO_ALQUIMICO = BLOCKS.register("caldero_alquimico",
-            () -> new DarkCalderoBlock(BlockBehaviour.Properties.copy(Blocks.STONE).noOcclusion().strength(2.0f))
+            () -> new DarkCalderoBlock(BlockBehaviour.Properties.of(net.minecraft.world.level.material.Material.STONE).noOcclusion().strength(2.0f))
     );
 
     public static void register(IEventBus eventBus) {
