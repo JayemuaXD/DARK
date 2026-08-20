522bf38f923f46a5ead9355903e20cd63165e39c
JayemuaXD <113012220+JayemuaXD@users.noreply.github.com>
2026-08-19 19:43:12 -0500

    Add ModBlocks (register caldero_alquimico)

---
+++ DARK/src/main/java/com/jayemuaxd/dark/core/registration/ModBlocks.java
@@ -0,0 +1,23 @@
+package com.jayemuaxd.dark.core.registration;
+
+import com.jayemuaxd.dark.core.DarkCoreMod;
+import com.jayemuaxd.dark.core.block.DarkCalderoBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, DarkCoreMod.MODID);
+
+    public static final RegistryObject<Block> CALDERO_ALQUIMICO = BLOCKS.register("caldero_alquimico",
+            () -> new DarkCalderoBlock(BlockBehaviour.Properties.of(Material.METAL).strength(2.0f).noOcclusion())
+    );
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
